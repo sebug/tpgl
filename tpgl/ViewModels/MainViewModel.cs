8598cc29c6b5f79928b4920d94887355b4106783
sebug <sebastian.gfeller@gmail.com>
2019-04-27 15:37:10 +0200

    First data binding

---
+++ tpgl/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace tpgl.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string message;
+
+        public string Message
+        {
+            get { return this.message; }
+            set
+            {
+                if (this.message != value)
+                {
+                    this.message = value;
+
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("message"));
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            this.Message = "tpgl from the view model";
+        }
+    }
+}
